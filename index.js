dc0fac11b812e178a1dd118277f11643be9a3f75
Ritesh512 <riteshprajapati1359@gmail.com>
2025-08-10 13:51:22 +0530

    Added cors fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // app.js
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import hrRoutes from './routes/hrRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -11,6 +12,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
